41eba6e3df09e494231e1a7aafa50d82bcc30dab
MananDesai54 <manan5401desai@gmail.com>
2020-11-17 18:14:19 +0530

    initial react & firebase setup

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./App.css";
+import { firebaseData } from "./firebase.config";
+
+function App() {
+  console.log(firebaseData);
+  return <div className="App">Hello Dlank</div>;
+}
+
+export default App;
